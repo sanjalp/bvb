ff25c84ca15f2e1ddb1d76799e472d90b6a8030f
sanjalp <103643278+sanjalp@users.noreply.github.com>
2022-07-02 13:16:10 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -6,7 +6,37 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
-function myFunction() {
+function myFunction2() {
    var element = document.body;
    element.classList.toggle("dark-mode");
+}
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+
+}
+function changeImage() {
+  var image = document.getElementById('myImage');
+  if (image.src.match("bulbon")) {
+    image.src = "light.png";
+  } else {
+    image.src = "dark.png";  }
+}
+
+function printPage() {
+  window.print();
 }
